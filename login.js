12f0773d8deb6affcec292a724dec619d940a291
NuonThanith <163116657+nounthanith@users.noreply.github.com>
2025-06-13 13:09:05 +0700

    update user login

---
+++ login.js
@@ -0,0 +1,39 @@
+const url =
+  "https://script.google.com/macros/s/AKfycbyycsdm_UzEGSSaXX60Vv2Od3kEOFUSE5jq8VEl4EvGoPfvX_4vbGwdyfAbClX_upv4/exec";
+
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("loginEmail").value;
+    const password = document.getElementById("loginPassword").value;
+    const loginMessage = document.getElementById("loginMessage");
+    // console.log(email, password);
+    // console.log(loginMessage.textContent)
+
+    try {
+      const res = await fetch(url + "?action=read");
+      const data = await res.json();
+      // console.log(data.data[0][2]);
+
+      for (let i = 0; i < data.data.length; i++) {
+        if (email === data.data[i][2] && password == data.data[i][3]) {
+          console.log("Login successful");
+          window.location.href = "user.html"; 
+          return;
+        }
+        if (email === data.data[i][2] && password != data.data[i][3]) {
+          console.log("Incorrect password");
+          loginMessage.textContent = "Incorrect password. Please try again.";
+          return;
+        }
+      }
+      
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      loginMessage.textContent =
+        "Error fetching user data. Please try again later.";
+      return;
+    }
+  });
---
+++ register.js
@@ -0,0 +1,34 @@
+const url =
+  "https://script.google.com/macros/s/AKfycbyycsdm_UzEGSSaXX60Vv2Od3kEOFUSE5jq8VEl4EvGoPfvX_4vbGwdyfAbClX_upv4/exec";
+  
+document.getElementById("userForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const role = document.getElementById("role").value;
+
+  const params = {
+    action: "insert",
+    id: Math.floor(Math.random() * 1000000),
+    name: name,
+    email: email,
+    password: password,
+    role: role,
+    created_at: new Date().toISOString(),
+  };
+
+  fetch(url + "?" + new URLSearchParams(params), { method: "POST" })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "success") {
+        // Redirect to login page
+        window.location.href = "login.html"; // Change to your login page path
+      } else {
+        alert("Registration failed. Please try again.");
+      }
+      document.getElementById("userForm").reset();
+      console.log(data);
+    });
+});
